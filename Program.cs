86db8bedbbdb615d95bf963235fb38b5b6d36d00
NeosNDG <148951847+NeosNDG@users.noreply.github.com>
2024-05-31 17:50:00 +0200

    INITIAL COMMIT TO LOCAL GIT

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using BlazingPizza.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+// Added by me: HTTP Client so that we can make HTTP requests (to the "specials" Controller API)
+// Added by me: Sqlite with the PizzaStoreContext so that we can use a database (create if not exists)
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
 var app = builder.Build();
 
@@ -16,5 +24,17 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Initialize the database via Data/SeedData.cs
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
